6b6ddbf5150803ed2ae11177f0af0d6ccd397d70
coded15 <g.ichchha@iitg.ac.in>
2023-05-23 16:04:49 +0530

    added "HelloWorldPanel"

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { HelloWorldPanel } from './HelloWorldPanel';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -19,7 +20,7 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.commands.registerCommand('vstodo.helloWorld', () => {
 			// The code you place here will be executed every time your command is executed
 			// Display a message box to the user
-			vscode.window.showInformationMessage('Hello from VSTodo!');
+			HelloWorldPanel.createOrShow(context.extensionUri);
 		})
 	);
 
